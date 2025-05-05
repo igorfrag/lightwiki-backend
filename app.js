189ecadfa85bbeb35364df1343d9660f2e3503b3
Igor Fragoso <igorfragoso3@hotmail.com>
2025-05-05 18:21:15 -0300

    listen all interfaces for docker compat

--- app.js
+++ app.js
@@ -117,6 +117,6 @@ app.delete('/api/delete/:id', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
